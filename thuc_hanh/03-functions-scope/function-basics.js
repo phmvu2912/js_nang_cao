cc0a6018a27828694c0b14094fc26dc4bcd4e342
phmvu2912 <phmvu2912@gmail.com>
2024-01-19 09:54:17 +0700

    update 19/01/24

---
+++ thuc_hanh/03-functions-scope/function-basics.js
@@ -0,0 +1,14 @@
+//Arrow functions
+const addNumber = (a, b) => {
+    return a + b;
+};
+
+//Implicit Return
+const subtract = (a, b) =>  a - b ;
+
+//Create an object
+const newObj = () => ({
+    name: 'Dao Vu',
+}); 
+
+console.log(newObj);
